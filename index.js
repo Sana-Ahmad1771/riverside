9ec3263ecfad4e5f8bee3f0148ee7e9d8f2bb2e0
Sana Ahmad <sanaahmad17771@gmail.com>
2025-06-24 15:35:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+  const hamburger = document.getElementById("open-menu");
+  const menu = document.getElementById("main-menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+ 
+//   function name(params) {
+    
+//   }
+
+//   ()=>{
+
+//   }
